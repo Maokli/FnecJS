2d54a5984cabe0f8b7107107404553270a520dfa
Maokli <louayinfo07@gmail.com>
2025-09-04 16:45:19 +0000

    put everything together

--- FnecJS/packages/fnec-scheduler/src/scheduler.ts
+++ FnecJS/packages/fnec-scheduler/src/scheduler.ts
@@ -3,6 +3,7 @@ import { Task } from "./types/task";
 import { TaskCallback } from "./types/taskCallback";
 import { PriorityLevel } from "./types/taskPriority";
 import { Thread } from "./types/taskThread";
+import { WorkLoop } from "./workLoop";
 
 /*
 * A stateful class that represents the scheduler.
@@ -17,9 +18,11 @@ export class FnecScheduler {
   /* An integer to track time */
   private timingService: TimingService = new TimingService();
 
+  private workLoop: WorkLoop = new WorkLoop();
+
   scheduleTask(callback: TaskCallback, priority: PriorityLevel, timeout: number, thread: Thread)
   {
-    const currentTIme = this.timingService.now()
+    const currentTIme = this.timingService.now();
 
     const task: Task = {
       callback: callback,
@@ -28,11 +31,18 @@ export class FnecScheduler {
       startTime: currentTIme,
       expiryTime: currentTIme + timeout,
       thread: thread
-    } 
+    }
 
     if(timeout > 0)
-      this.timerQueue.push(task)
+      this.timerQueue.push(task);
     else
-      this.tasksQueue.push(task)
+      this.tasksQueue.push(task);
+  }
+
+  startWork() {
+    while(this.tasksQueue.length > 0 && this.timerQueue.length > 0) {
+      this.timingService.advanceTime(this.timerQueue, this.tasksQueue);
+      this.workLoop.DoWork(this.tasksQueue);
+    }
   }
 }
